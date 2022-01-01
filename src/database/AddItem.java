610f0145edcb656e402503260d468bbf98fd5f9b
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-01-01 15:00:56 +0100

    Added addStudent to AddItem.

--- src/database/AddItem.java
+++ src/database/AddItem.java
@@ -48,4 +48,46 @@ public class AddItem {
 
     }
 
+    public static void addStudent(String email, String name, String dateOfBirth, String gender, String address,
+                                  String city, String country) {
+        // These are the settings for the connection.
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;";
+
+        // Connection controls information about the connection to the database.
+        Connection con = null;
+
+        // Statement lets us use SQL query's.
+        Statement stmt = null;
+
+        // ResultSet is the table we get from the database.
+        // We can iterate through the rows.
+        ResultSet rs = null;
+
+        try {
+            // Importing driver...
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Connecting to the database...
+            con = DriverManager.getConnection(connectionUrl);
+
+            // Making a SQL query.
+            String SQL = "INSERT Cursist VALUES ('" + email + "', '" + name + "', '" + dateOfBirth + "', '"
+                    + gender + "', '" + address + "', '" + city + "', '" + country + "')";
+            stmt = con.createStatement();
+            // Executing the query in the database
+            rs = stmt.executeQuery(SQL);
+
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+            if (con != null) try { con.close(); } catch(Exception e) {}
+        }
+
+    }
+
 }

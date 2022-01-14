008294d5914299fde045568e110e6730d03ffea4
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-01-14 12:15:22 +0100

    Simplified imports.

--- src/database/GetPercentageByGender.java
+++ src/database/GetPercentageByGender.java
@@ -1,10 +1,7 @@
 package database;
 
 import userdata.CertificateAndStudent;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 

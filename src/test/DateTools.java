548215f0a4a482d789ab8062b6356ae7e21173ee
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-01-15 20:17:20 +0100

    Removed redundant check in DateTools.

--- src/test/DateTools.java
+++ src/test/DateTools.java
@@ -40,9 +40,6 @@ public class DateTools {
         } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10
                 || month == 12 && day >= 1 || day <= 31) {
             return "true";
-        } else if ((month == 4 || month == 2 || month == 7 || month == 6 || month == 9 || month == 10 || month == 12)
-                && day >= 1 && day <= 31) {
-            return "false";
         } else if (month == 4 || month == 6 || month == 9 || month == 11 && day >= 1 || day <= 30) {
             return "true";
         } else if (month == 2 && day >= 1 || day <= 29 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {

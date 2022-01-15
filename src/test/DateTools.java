6acff63ec19d904c70fdd1a7198ecfdda2a68381
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2022-01-15 22:55:56 +0100

    Changed DateTools validation to also check if the year is bigger than 9999.

--- src/test/DateTools.java
+++ src/test/DateTools.java
@@ -31,6 +31,12 @@ public class DateTools {
      *  @requires month > 12;
      * @ensures \result = false;
      *          }
+     *
+     * @subcontract year is 11111 {
+     *  @requires year > 9999;
+     * @ensures \result = false;
+     *          }
+     *
      * @subcontract all other cases {
      * @requires no other accepting precondition;
      * @ensures \result = false;
@@ -42,11 +48,12 @@ public class DateTools {
             return "false";
         } else if (month > 12) {
             return "false";
+        } else if (year > 9999) {
+            return "false";
         } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10
-                || month == 12 && day >= 1 || day <= 31) {
+                || month == 12 && day >= 1 && day <= 31) {
             return "true";
-        }
-        else if (month == 4 || month == 6 || month == 9 || month == 11 && day >= 1 || day <= 30) {
+        } else if (month == 4 || month == 6 || month == 9 || month == 11 && day >= 1 || day <= 30) {
             return "true";
         } else if (month == 2 && day >= 1 || day <= 29 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {
             return "true";
